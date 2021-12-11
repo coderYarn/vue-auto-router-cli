450c9e628ddf22cdae6fee50a2bbc423ec81237b
coderYarn <642997741@qq.com>
2021-12-11 18:35:34 +0800

    ✨ feat(lib): write node

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,37 @@
+
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+export default new Router({
+mode: 'history',
+base: process.env.BASE_URL,
+routes: [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About/index.vue'),
+    
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home/index.vue'),
+    
+      children:[
+  {
+    path: '/home/hello',
+    name: 'Hello',
+    component: () => import('../views/Home/Hello/index.vue'),
   }
+      ]
+  },
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('../views/Index/index.vue'),
+    
+  },
 ]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
 })
-
-export default router
